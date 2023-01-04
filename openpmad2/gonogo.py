46d75fbc15f84ff83ce4a4c14dd63d206705e363
jbhunt <hunt.brian.joshua@gmail.com>
2023-01-04 07:43:49 -0800

    Created a new protocol for the go/no-go task

--- openpmad2/gonogo.py
+++ openpmad2/gonogo.py
@@ -185,3 +185,146 @@ class StaticGratingWithVariableProbe():
                 stream.write(f'{trialNumber:.0f}, {probeContrast:.3f}, {timestamp:.3f}\n')
 
         return
+
+class DriftingGratingWithVariableProbe():
+    """
+    """
+
+    def __init__(self, display):
+        """
+        """
+
+        self.display = display
+        self.metadata = None
+
+        return
+
+    def present(
+        self,
+        spatialFrequency=0.08,
+        velocity=12,
+        probeDuration=0.5,
+        blockDuration=5,
+        nBlocksPerDirection=1,
+        probeContrastLevels=(1, 1),
+        probeContrastProbabilities=(0.5, 0.5),
+        baselineContrastLevel=0.5,
+        interProbeIntervalRange=(1, 3),
+        interBlockInterval=5,
+        buffers=(0.5, 0.5),
+        ):
+        """
+        """
+
+        #
+        if len(probeContrastLevels) != len(probeContrastProbabilities):
+            raise Exception('Number of probe levels must equal the number of probabilities')
+
+        if np.sum(probeContrastProbabilities) != 1:
+            raise Exception('Probe probabilities must sum to 1')
+
+        #
+        self.metadata = np.full([1000000, 4], np.nan)
+
+        #
+        cpp = spatialFrequency / self.display.ppd # cycles per pixel
+        cpf = spatialFrequency * velocity / self.display.fps
+        gabor = visual.GratingStim(
+            self.display,
+            size=self.display.size,
+            units='pix',
+            sf=cpp,
+            contrast=baselineContrastLevel,
+        )
+
+        #
+        directions = np.tile([-1, 1], nBlocksPerDirection)
+        np.random.shuffle(directions)
+        countdown = int(np.ceil(np.random.uniform(*interProbeIntervalRange, size=1).item()))
+        trialIndex = 0
+        presentingProbe = False
+        recordTimestamp = False
+
+        for direction in directions:
+
+            # Show grating static for 3 seconds
+            for iFrame in range(int(np.ceil(3 * self.display.fps))):
+                gabor.draw()
+                self.display.flip()
+
+            #
+            nFrames = int(np.ceil(blockDuration * self.display.fps))
+            boundaries = (
+                0 + int(np.ceil(buffers[0] * self.display.fps)),
+                nFrames - int(np.ceil(buffers[1] * self.display.fps))
+            )
+            probeDurationInFrames = int(np.ceil(probeDuration * self.display.fps))
+            for iFrame in range(nFrames):
+
+                #
+                if countdown == 0:
+
+                    # Exit probe phase
+                    if presentingProbe:
+
+                        # Choose an new inter-probe interval
+                        countdown = int(np.ceil(np.random.uniform(*interProbeIntervalRange, size=1).item() * self.display.fps))
+                        gabor.contrast = baselineContrastLevel
+                        presentingProbe = False
+
+                    # Attempt to enter probe phase
+                    else:
+
+                        # Make sure the probe onset and offset are within the temporal boundaries
+                        if iFrame < boundaries[0] or iFrame + probeDurationInFrames > boundaries[1]:
+                            countdown = 1 # Keep reseting the countdown to 1
+
+                        # Present the probe
+                        else:
+                            self.display.signalEvent(3, units='frames')
+                            gabor.contrast = np.random.choice(probeContrastLevels, p=probeContrastProbabilities)
+                            self.metadata[trialIndex, 0] = trialIndex + 1
+                            self.metadata[trialIndex, 1] = gabor.contrast
+                            self.metadata[trialIndex, 2] = direction
+                            countdown = int(np.ceil(probeDuration * self.display.fps))
+                            presentingProbe = True
+                            recordTimestamp = True
+    
+                #
+                gabor.draw()
+                timestamp = self.display.flip()
+                if recordTimestamp:
+                    self.metadata[trialIndex, 3] = timestamp
+                    trialIndex += 1 # Increment the trial index
+                    recordTimestamp = False
+                gabor.phase += direction * cpf
+                countdown -= 1
+            
+            # Inter-block interval
+            self.display.idle(interBlockInterval, units='seconds')
+
+        #
+        mask = np.invert(np.isnan(self.metadata).all(axis=1))
+        self.metadata = self.metadata[mask, :]
+
+        return
+
+    def saveMetadata(self, sessionFolder):
+        """
+        """
+
+        sessionFolderPath = pl.Path(sessionFolder)
+
+        if self.metadata is None:
+            return
+
+        if sessionFolderPath.exists() == False:
+            sessionFolderPath.mkdir()
+
+        filename = str(sessionFolderPath.joinpath('driftingGratingWithProbeMetadata.txt'))
+        with open(filename, 'w') as stream:
+            stream.write(f'Trial number, Probe contrast, Motion\n')
+            for trialNumber, probeContrast, timestamp in self.metadata:
+                stream.write(f'{trialNumber:.0f}, {probeContrast:.3f}, {timestamp:.3f}\n')
+
+        return
